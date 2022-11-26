951c7f1b01446f8e86fdb511451161971a103e1b
Umamad <marandmohammad@gmail.com>
2022-11-27 02:07:57 +0330

    start using mongoDB

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -3,7 +3,7 @@ const {
   addNewLaunch,
   existsWithId,
   abortLaunchById,
-} = require("../../models/launches.model");
+} = require("../../models/launches/launches.model");
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,22 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
-const { loadPlanetsData } = require("./models/planets.model");
+const { loadPlanetsData } = require("./models/planets/planets.model");
 
+//? Definitions
 const PORT = process.env.PORT || 8000;
+const CONNECTION_STRING = `mongodb+srv://nasa-api:S65549y4oeYFBegp@nasacluster.wbwcsju.mongodb.net/nasa?retryWrites=true&w=majority`;
 
 const server = http.createServer(app);
 
+//? Data base events
+mongoose.connection.once("open", () => console.log("Connected to DB!"));
+mongoose.connection.on("error", (error) => console.error(error));
+
 async function startServerWithAfterDataLoad() {
+  await mongoose.connect(CONNECTION_STRING);
+
   // Load planets data
   await loadPlanetsData();
 
